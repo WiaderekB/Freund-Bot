f1d3225a39032eba79d8047c97e095348fa7f0af
Bartosz Wiaderek <64980866+Mixeronix@users.noreply.github.com>
2022-12-13 21:10:24 +0100

    Kill function

--- src/commands.js
+++ src/commands.js
@@ -2,9 +2,7 @@ import { SlashCommandBuilder } from "discord.js";
 
 const ping = new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!");
 
-const meme = new SlashCommandBuilder()
-	.setName("meme")
-	.setDescription("Freund will make you HAPPY! ...with not funny meme.");
+const meme = new SlashCommandBuilder().setName("meme").setDescription("Freund will make you HAPPY! ...with not funny meme.");
 
 const dice = new SlashCommandBuilder()
 	.setName("dice")
@@ -13,5 +11,10 @@ const dice = new SlashCommandBuilder()
 		return option.setName("d").setDescription("Dice size. 😑").setRequired(true);
 	});
 
-const commands = [ping, meme, dice];
+const kill = new SlashCommandBuilder()
+	.setName("kill")
+	.setDescription("Freund will kill someone for you!")
+	.addMentionableOption((option) => option.setName("person").setDescription("Who do you wanna kill???").setRequired(true));
+
+const commands = [ping, meme, dice, kill];
 export default commands;
--- src/index.js
+++ src/index.js
@@ -11,19 +11,33 @@ client.once(Events.ClientReady, async (c) => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+function timeConverter(UNIX_timestamp) {
+	var a = new Date(UNIX_timestamp);
+	var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	var year = a.getFullYear();
+	var month = months[a.getMonth()];
+	var date = a.getDate();
+	var hour = a.getHours();
+	var min = a.getMinutes();
+	var sec = a.getSeconds();
+
+	if (hour < 10) hour = `0${hour}`;
+	if (min < 10) min = `0${min}`;
+	if (sec < 10) sec = `0${sec}`;
+	if (date < 10) date = `0${date}`;
+
+	var time = hour + ":" + min + ":" + sec + " | " + date + " " + month + " " + year + " ";
+	return time;
+}
+
 async function main() {
 	try {
-		await rest.put(
-			Routes.applicationGuildCommands("1051643493577130004", "1051649341137354874"),
-			{ body: commands }
-		);
+		await rest.put(Routes.applicationGuildCommands("1051643493577130004", "1051649341137354874"), { body: commands });
 		client.login(token.token);
 	} catch (e) {
 		console.error(e);
 	}
 }
-
-main();
 
 client.on(Events.InteractionCreate, (interaction) => {
 	if (!interaction.isChatInputCommand()) return;
@@ -38,20 +52,51 @@ client.on(Events.InteractionCreate, (interaction) => {
 		case "dice":
 			dice(interaction);
 			break;
+		case "kill":
+			kill(interaction);
+			break;
 	}
 });
 
 function ping(i) {
 	i.reply("Pong!");
+
+	console.log(
+		`${timeConverter(i.createdTimestamp)}
+		${i.user.username}#${i.user.discriminator} pinged`
+	);
 }
 
 async function meme(i) {
 	const img = await (await fetch("https://meme-api.com/gimme")).json();
 
 	i.reply(img.url);
+
+	console.log(
+		`${timeConverter(i.createdTimestamp)}
+		${i.user.username}#${i.user.discriminator} asked for a meme`
+	);
 }
 
 function dice(i) {
 	let number = Math.floor(Math.random() * i.options.get("d").value.toString()) + 1;
 	i.reply(number.toString());
+
+	console.log(
+		`${timeConverter(i.createdTimestamp)}
+		${i.user.username}#${i.user.discriminator} rolled d${i.options.get("d").value} and got ${number}`
+	);
 }
+
+async function kill(i) {
+	const gif_result = await (await fetch("https://g.tenor.com/v1/random?&q=killing&key=LIVDSRZULELA&limit=1")).json();
+
+	i.reply(gif_result.results[0].url);
+
+	console.log(
+		`${timeConverter(i.createdTimestamp)}
+		${i.user.username}#${i.user.discriminator} killed ${i.options.get("person").user.username}#${i.options.get("person").user.discriminator}`
+	);
+}
+
+main();
