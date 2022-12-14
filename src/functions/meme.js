dd82755f500f3f62bca8e772d07cd09a6ef2ea50
Bartosz Wiaderek <64980866+Mixeronix@users.noreply.github.com>
2022-12-14 20:41:00 +0100

    Embed meme

--- src/functions/meme.js
+++ src/functions/meme.js
@@ -1,10 +1,22 @@
 import { timeConverter } from "./timeConverter.js";
 import fetch from "node-fetch";
+import { EmbedBuilder } from "discord.js";
 
 export async function meme(i) {
 	const img = await (await fetch("https://meme-api.com/gimme")).json();
 
-	i.reply(img.url);
+	const embed = new EmbedBuilder()
+		.setColor(0x809696)
+		.setTitle("That'a MEME!!")
+		.setAuthor({ name: "YOU! 😁", iconURL: `https://cdn.discordapp.com/avatars/${i.user.id}/${i.user.avatar}.png` })
+		.setTimestamp()
+		.setImage(img.url)
+		.setFooter({
+			text: "Hope it made you LAUGH! 📇",
+			iconURL: "https://cdn.discordapp.com/avatars/1051643493577130004/5e3ac8379ae3ceb815c8d746b25db9f0.png",
+		});
+
+	i.reply({ embeds: [embed] });
 
 	console.log(
 		`${timeConverter(i.createdTimestamp)}
